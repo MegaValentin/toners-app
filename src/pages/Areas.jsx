ec67470f0c0facc8b2ea23073174d7852179968e
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2024-05-30 13:46:29 -0300

    areausage

--- src/pages/Areas.jsx
+++ src/pages/Areas.jsx
@@ -20,12 +20,12 @@ const Areas = () => {
   return (
     <div className='bg-transparent p-8 rounded-lg w-full mt-10'>
       <h1 className="text-3xl font-bold mb-4">Gestión de Áreas</h1>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6'>
         {areas.map((area) => (
-          <div key={area._id} className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
-            <h2 className='mb-2 text-xl md:text-2xl lg:text-3xl font-bold tracking-tight text-gray-900 dark:text-white uppercase'>{area.areaName}</h2>
+          <div key={area._id} className=' max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow '>
+            <h2 className='mb-2 text-xl md:text-2xl lg:text-1xl font-bold tracking-tight text-gray-900 text-center uppercase'>{area.areaName}</h2>
             <div>
-              <h3 className='mb-3 text-sm md:text-base lg:text-lg font-normal text-gray-700 dark:text-gray-400'>Toners:</h3>
+              <h3 className='mb-3 text-sm md:text-base lg:text-lg font-normal text-gray-700 '>Toners:</h3>
               <ul>
                 {area.toners.map((toner) => (
                   <li key={toner.toner}>
