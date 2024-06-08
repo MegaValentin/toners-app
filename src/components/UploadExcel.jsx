f1d12baab49dfbdd7e4010aebb958ad885681978
unknown <valentimega@gmail.com>
2024-06-08 20:45:03 -0300

    check data

--- src/components/UploadExcel.jsx
+++ src/components/UploadExcel.jsx
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import TonersTable from './TonerTable';
 const UploadExcel = () => {
   const [file, setFile] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadError, setUploadError] = useState(null);
+  const [dataExists, setDataExists] = useState(false);
+
+  useEffect(() => {
+    const checkData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3500/api/checkdata');
+        setDataExists(response.data.dataExists);
+      } catch (error) {
+        console.error('Error checking data:', error);
+      }
+    };
+
+    checkData();
+  }, []);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -17,6 +33,8 @@ const UploadExcel = () => {
 
     const formData = new FormData();
     formData.append('file', file);
+    setIsUploading(true);
+    setUploadError(null);
 
     try {
       const response = await axios.post('http://localhost:3500/api/addstockideal', formData, {
@@ -25,17 +43,42 @@ const UploadExcel = () => {
         },
       });
       alert('File uploaded successfully!');
+      window.location.reload();
     } catch (error) {
       console.error('Error uploading file:', error);
-      alert('Failed to upload file.');
+      
+      setUploadError('Failed to upload file.');
+    }finally {
+      setIsUploading(false);
     }
   };
 
+  if (dataExists) {
+    return (
+      <div>
+        <TonersTable/>
+      </div>
+    );
+  }
   return (
-    <div className="upload-excel">
-      <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileChange} />
-        <button type="submit">Upload Excel</button>
+    <div className="flex flex-col items-center justify-center ">
+      <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
+        
+        <div className="mb-4">
+          <input 
+            type="file" 
+            onChange={handleFileChange} 
+            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100" 
+          />
+        </div>
+        <button 
+          type="submit" 
+          className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 disabled:opacity-50" 
+          disabled={isUploading}
+        >
+          {isUploading ? 'Cargando...' : 'Cargar Excel'}
+        </button>
+        {uploadError && <p className="mt-4 text-red-600">{uploadError}</p>}
       </form>
     </div>
   );
