acc2f3eec06f8e4cce0c47d014406461b3cc941c
unknown <valentimega@gmail.com>
2024-06-08 19:45:36 -0300

    solucion lectura de excel

--- src/components/UploadExcel.jsx
+++ src/components/UploadExcel.jsx
@@ -2,35 +2,34 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const UploadExcel = () => {
-    const [file, setFile] = useState(null);
+  const [file, setFile] = useState(null);
 
-    const handleFileChange = (e) => {
-        setFile(e.target.files[0]);
-        console.log(setFile)
-      };
-    
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (!file) {
-          alert('Please select a file first!');
-          return;
-        }
-    
-        const formData = new FormData();
-        formData.append('file', file);
-    
-        try {
-          await axios.post('http://localhost:3500/api/addstockideal', formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-          });
-          alert('File uploaded successfully!');
-        } catch (error) {
-          console.error('Error uploading file:', error);
-          alert('Failed to upload file.');
-        }
-      };
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!file) {
+      alert('Please select a file first!');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await axios.post('http://localhost:3500/api/addstockideal', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      alert('File uploaded successfully!');
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      alert('Failed to upload file.');
+    }
+  };
 
   return (
     <div className="upload-excel">
@@ -39,8 +38,7 @@ const UploadExcel = () => {
         <button type="submit">Upload Excel</button>
       </form>
     </div>
-
-  )
+  );
 };
 
 export default UploadExcel;
