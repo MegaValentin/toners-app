997a4e8e91bc74e50a80b0b0170f0562d4d7a77e
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2024-08-20 10:27:50 -0300

    history orders

--- src/components/HistoryOrders.jsx
+++ src/components/HistoryOrders.jsx
@@ -62,7 +62,7 @@ const HistoryOrders = () => {
                 <div className="flex items-center">Estado</div>
               </th>
               <th scope="col" className="px-6 py-3">
-                  <div className="flex items-center">Acciones</div>
+                  <div className="flex items-center"></div>
                 </th>
             </tr>
           </thead>
@@ -81,10 +81,18 @@ const HistoryOrders = () => {
                     {orders.isDelivered && (
                       <button
                         onClick={() => handleCancelOrder(orders._id)}
-                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="bg-transparent text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         disabled={loading}
                       >
-                        Cancelar
+                        <svg  xmlns="http://www.w3.org/2000/svg"  
+                        width="24"  
+                        height="24"  
+                        viewBox="0 0 24 24"  
+                        fill="currentColor"  
+                        className="icon icon-tabler icons-tabler-filled icon-tabler-circle-x text-red-500 hover:text-red-700">
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                          <path d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-6.489 5.8a1 1 0 0 0 -1.218 1.567l1.292 1.293l-1.292 1.293l-.083 .094a1 1 0 0 0 1.497 1.32l1.293 -1.292l1.293 1.292l.094 .083a1 1 0 0 0 1.32 -1.497l-1.292 -1.293l1.292 -1.293l.083 -.094a1 1 0 0 0 -1.497 -1.32l-1.293 1.292l-1.293 -1.292l-.094 -.083z" />
+                        </svg>
                       </button>
                     )}
                   </td>
