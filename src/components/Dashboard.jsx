3b47da3a28e82485208d6ac443838d228487b5dd
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2024-10-30 12:01:17 -0300

    dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -85,121 +85,140 @@ const Dashborad = () => {
   ]
 
 
-  const renderLinks = (links) => (
-    links.map(link => (
+  const renderLinks = (links) =>
+    links.map((link, index) => (
       <Link
+        key={index}
         to={link.to}
-        className="text-white mb-4 border-black/40 flex justify-between text-xs lg:text-base gap-x-2 py-1 px-2 lg:py-2 md:px-4 hover:bg-gray-300"
+        className="flex items-center text-white text-sm lg:text-base gap-x-2 py-2 px-4 hover:bg-gray-300"
         onClick={link.click}
       >
-        {link.label}
         {link.icon}
+        {link.label}
       </Link>
-    ))
-  );
+    ));
 
   return (
     <nav className="bg-gray-600 w-full lg:w-64 flex flex-col p-4 lg:h-full">
-      <div className="flex justify-between lg:justify-center items-center mb-10">
-        <Link to="/" className="flex justify-center items-center">
-          <img src={logoMuni} className="size-10 lg:size-20" alt="Logo" />
-        </Link>
-        <button
-          className="lg:hidden block text-white"
-          onClick={() => setMenuOpen(!menuOpen)}
+    <div className="flex justify-between items-center mb-4 lg:mb-10">
+      <Link to="/" className="flex justify-center items-center">
+        <img src={logoMuni} className="h-10 lg:h-20" alt="Logo" />
+      </Link>
+      <button
+        className="lg:hidden block text-white"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
-      </div>
-      {
-        isAuthenticated ? (
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+      </button>
+    </div>
+
+    {isAuthenticated && (
+      <div className={`lg:flex flex-col ${menuOpen ? "block" : "hidden"}`}>
+        {user.role === "superadmin" && (
           <>
-            {user.role === "superadmin" && (
-              <>
-                <div className={`lg:flex flex-col ${menuOpen ? "block" : "hidden"}`}>
-
-                  <div className="relative mb-4">
-                    <button
-                      className="rounded-lg text-white border-black/40 flex lg:justify-between justify-center text-xs lg:text-base gap-x-2 py-1 px-2 lg:py-2 md:px-4 hover:bg-gray-300 w-full"
-                      onClick={toggleDropdownTareas}
-                    >
-                      Tareas
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`transform ${dropdownOpenTareas ? 'rotate-180' : 'rotate-0'} transition-transform duration-200`}><path stroke="none" d="M0 0h24h24H0z" fill="none" /><path d="M6 9l6 6l6 -6" /></svg>
-                    </button>
-                    {dropdownOpenTareas && (
-                      <div className="absolute left-full top-0 mt-2 w-48 bg-gray-600 text-white z-10">
-                        {renderLinks(taskLinks)}
-                      </div>
-                    )}
-                  </div>
-
-                  <div className="relative mb-4">
-                    <button
-                      className="rounded-lg text-white border-black/40 flex lg:justify-between justify-center text-xs lg:text-base gap-x-2 py-1 px-2 lg:py-2 md:px-4 hover:bg-gray-300 w-full"
-                      onClick={toggleDropdown}
-                    >
-                      Toners
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`transform ${dropdownOpen ? 'rotate-180' : 'rotate-0'} transition-transform duration-200`}><path stroke="none" d="M0 0h24h24H0z" fill="none" /><path d="M6 9l6 6l6 -6" /></svg>
-                    </button>
-                    {dropdownOpen && (
-                      <div className="absolute left-full top-0 mt-2 w-48 bg-gray-600 text-white z-10">
-                        
-                        {renderLinks(tonersLinks)}
-                      </div>
-                    )}
-                  </div>
-
-                  {renderLinks(superAdminLinks)}
-                </div>
-              </>
-            )}
-            {user.role === 'admin' && (
-              <div className={`lg:flex flex-col ${menuOpen ? "block" : "hidden"}`}>
-                <div className="relative mb-4">
-                <button
-                  className="rounded-lg text-white border-black/40 flex lg:justify-between justify-center text-xs lg:text-base gap-x-2 py-1 px-2 lg:py-2 md:px-4 hover:bg-gray-300 w-full"
-                  onClick={toggleDropdown}
+            <div className="relative mb-4">
+              <button
+                className="flex justify-between items-center text-white py-2 px-4 w-full hover:bg-gray-700"
+                onClick={toggleDropdownTareas}
+              >
+                Tareas
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className={`transition-transform duration-200 ${
+                    dropdownOpenTareas ? "rotate-180" : "rotate-0"
+                  }`}
                 >
-                  Stock
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`transform ${dropdownOpen ? 'rotate-180' : 'rotate-0'} transition-transform duration-200`}><path stroke="none" d="M0 0h24h24H0z" fill="none" /><path d="M6 9l6 6l6 -6" /></svg>
-                </button>
-                {dropdownOpen && (
-                  <div className="absolute left-full top-0 mt-2 w-48 bg-gray-600 text-white z-10">
-                    {renderLinks(stockLinks)}
-                  </div>
-                )}
-              </div>
-              {renderLinks(adminLinks)}
+                  <path d="M6 9l6 6 6-6" />
+                </svg>
+              </button>
+              {dropdownOpenTareas && (
+                <div className="pl-4 text-white bg-gray-700">{renderLinks(taskLinks)}</div>
+              )}
             </div>
-            )}
-            {user.role === 'empleado' && (
-              <>
-                <div className={`lg:flex flex-col ${menuOpen ? "block" : "hidden"}`}>
-                {renderLinks(empleadoLinks)}
-                </div>
 
-              </>
-            )}
+            <div className="relative mb-4">
+              <button
+                className="flex justify-between items-center text-white py-2 px-4 w-full hover:bg-gray-700"
+                onClick={toggleDropdown}
+              >
+                Toners
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className={`transition-transform duration-200 ${
+                    dropdownOpen ? "rotate-180" : "rotate-0"
+                  }`}
+                >
+                  <path d="M6 9l6 6 6-6" />
+                </svg>
+              </button>
+              {dropdownOpen && (
+                <div className="pl-4 text-white bg-gray-700">{renderLinks(tonersLinks)}</div>
+              )}
+            </div>
 
-          </>
-        ) : (
-          <>
+            {renderLinks(superAdminLinks)}
           </>
         )}
-    </nav>
+
+        {user.role === "admin" && (
+          <div className="relative mb-4">
+            <button
+              className="flex justify-between items-center text-white py-2 px-4 w-full hover:bg-gray-700"
+              onClick={toggleDropdown}
+            >
+              Stock
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className={`transition-transform duration-200 ${
+                  dropdownOpen ? "rotate-180" : "rotate-0"
+                }`}
+              >
+                <path d="M6 9l6 6 6-6" />
+              </svg>
+            </button>
+            {dropdownOpen && (
+              <div className="pl-4 text-white bg-gray-700">{renderLinks(stockLinks)}</div>
+            )}
+            {renderLinks(adminLinks)}
+          </div>
+        )}
+
+        {user.role === "empleado" && <div>{renderLinks(empleadoLinks)}</div>}
+      </div>
+    )}
+  </nav>
   )
 }
 
